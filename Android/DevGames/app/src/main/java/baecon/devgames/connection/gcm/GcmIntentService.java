a086046462329ea8346f28193e3fbaf53df27526
mjollnir94 <marcel_Hollink@hotmail.com>
2016-03-06 16:13:28 +0100

    Added GCM basic stuff to the App

---
+++ Android/DevGames/app/src/main/java/baecon/devgames/connection/gcm/GcmIntentService.java
@@ -0,0 +1,17 @@
+package baecon.devgames.connection.gcm;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class GcmIntentService extends IntentService{
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
---
+++ Android/DevGames/app/src/main/java/baecon/devgames/connection/gcm/GumBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package baecon.devgames.connection.gcm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GumBroadcastReceiver extends WakefulBroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
