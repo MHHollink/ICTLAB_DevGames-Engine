10547aaf734d21f108127099e3174ba09768f8f8
mjollnir94 <marcel_Hollink@hotmail.com>
2016-04-24 21:39:21 +0200

    get on pushes

--- SpringServer/DevGamesSpringServer/src/main/java/nl/devgames/rest/controller/PushController.java
+++ SpringServer/DevGamesSpringServer/src/main/java/nl/devgames/rest/controller/PushController.java
@@ -13,6 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/pushes")
 public class PushController extends BaseController {
 
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Push getPushes(@RequestHeader(value = Application.SESSION_HEADER_KEY) String session,
+                            @PathVariable long id)
+    {
+        throw new UnsupportedOperationException();
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Push getPushById(@RequestHeader(value = Application.SESSION_HEADER_KEY) String session,
                             @PathVariable long id)
