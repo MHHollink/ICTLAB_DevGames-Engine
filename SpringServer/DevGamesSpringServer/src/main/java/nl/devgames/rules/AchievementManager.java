3f8053a4deae755f93379253abdcc60fe0cf2b37
jorikito <jorikpaul@hotmail.com>
2016-06-22 17:47:16 +0200

    achievement check send gcm

--- SpringServer/DevGamesSpringServer/src/main/java/nl/devgames/rules/AchievementManager.java
+++ SpringServer/DevGamesSpringServer/src/main/java/nl/devgames/rules/AchievementManager.java
@@ -1,18 +1,26 @@
 package nl.devgames.rules;
 
+import nl.devgames.connection.database.dao.PushDao;
 import nl.devgames.connection.gcm.GCMMessageComposer;
 import nl.devgames.connection.gcm.GCMMessageType;
+import nl.devgames.model.Push;
 import nl.devgames.model.User;
 import nl.devgames.utils.L;
+
+import java.net.ConnectException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jorikito on 02-Jun-16.
  */
 public class AchievementManager {
     private User user;
+    PushDao pushDao;
 
     public AchievementManager(User user) {
         this.user = user;
+        pushDao = new PushDao();
     }
 
     private void sendAchievementMessage(Integer achievementId) {
@@ -25,7 +33,27 @@ public class AchievementManager {
         );
     }
 
-    public void checkAchievementsOfUser() {
+    public void checkAchievementsOfUser() throws ConnectException {
+        checkTwoPushesIn10MinutesAchievements();
+    }
+
+    private void checkTwoPushesIn10MinutesAchievements() throws ConnectException {
+        //cehck if already achieved
+
+
+        Long currentTime = System.currentTimeMillis();
+        List<Push> pushesInLast10Minutes = new ArrayList<>();
+
+        List<Push> userPushes = pushDao.queryByUser(user.getId());
+        for(Push push : userPushes) {
+            if(push.getTimestamp() > currentTime - (1000 * 60 * 10)) {
+                pushesInLast10Minutes.add(push);
+            }
+        }
+        if(pushesInLast10Minutes.size() > 2 ) {
+            //achievement unlocked
+
+        }
 
     }
 }
